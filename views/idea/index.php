24818db7a49446b83934c98ac656a2dc085d92ac
Russell Purinton <russell.purinton@gmail.com>
2023-08-16 05:15:30 -0400

    Update index.php

--- views/idea/index.php
+++ views/idea/index.php
@@ -6,16 +6,30 @@ require_once(__DIR__ . '/../src/Session.php');
 $session = new Session(false);
 $idea_id = explode('/', $_SERVER['REQUEST_URI'])[2];
 extract($session->sql->single("SELECT min(`idea_id`) as `min_id`, max(`idea_id`) as `max_id` FROM `ideas`"));
-if (!is_numeric($idea_id) || $idea_id < $min_id || $idea_id > $max_id) {
-    $session->redirect('/search');
-}
-$session->header("Idea $idea_id");
+if (!is_numeric($idea_id) || $idea_id < $min_id || $idea_id > $max_id) $session->redirect('/search');
+$result = $sql->single("SELECT * FROM `ideas` WHERE `idea_id` = '$idea_id'");
+if (!$result) $session->redirect('/search');
+extract($result);
+$session->header("Idea #$idea_id");
 ?>
 <style>
 
 </style>
 <div class="discroller">
     <h1>Idea #<?= $idea_id ?></h1>
+    <div class="idea">
+        <div class="idea-description"><?= $description ?></div>
+        <div class="idea-tags">
+            <?php
+            $tags = $sql->query("SELECT `tag` FROM `tags` WHERE `idea_id` = $idea_id");
+            while ($tag = $tags->fetch_assoc()) echo "<div class='idea-tag'>{$tag['tag']}</div>";
+            ?>
+        </div>
+        <div class="idea-votes">
+            <div class="idea-vote" data-vote="up" data-idea="<?= $idea_id ?>">Up</div>
+            <div class="idea-vote" data-vote="down" data-idea="<?= $idea_id ?>">Down</div>
+        </div>
+    </div>
 </div>
 <script>
 
