d9c53ad891a6a4a5a49bb8caafd1a83ac5811f73
Russell Purinton <russell.purinton@gmail.com>
2023-08-16 01:16:09 -0400

    Update index.php

--- views/new-idea/index.php
+++ views/new-idea/index.php
@@ -50,7 +50,7 @@ $session->header("Idea Submission");
         -webkit-appearance: none;
         appearance: none;
         color: #f0f8ff;
-        font-size: 150%;
+        font-size: 125%;
         font-weight: bold;
         text-align-last: center;
         line-height: 100%;
@@ -61,17 +61,17 @@ $session->header("Idea Submission");
     <div class="form-check form-switch control-group">
         <div class="row">
             <div class="col-auto" style='display: flex; align-items: center;'>
-                <label class="form-check-label" for="privacy-toggle" style='font-size:150%; font-weight:bold;'>Private</label>
+                <label class="form-check-label" for="privacy-toggle" style='font-size:125%; font-weight:bold;'>Private</label>
             </div>
             <div class="col" style='display: flex; align-items: center;'>
                 <input class="form-check-input" type="checkbox" id="privacy-toggle" name="privacy-toggle">
             </div>
             <div class="col-auto" style='display: flex; align-items: center;'>
-                <label id="public-label" class="form-check-label" for="privacy-toggle" style='font-size:150%; font-weight:bold;'>Public</label>
+                <label id="public-label" class="form-check-label" for="privacy-toggle" style='font-size:125%; font-weight:bold;'>Public</label>
             </div>
             <div class="col-auto" id="license-dropdwn" style='display: flex; visibility:hidden; align-items: center;'>
                 <!-- dropdown box listing top 10 common public licenses -->
-                <select class="form-select form-control" aria-label="License" style='max-width: 50px; color:#f0f8ff;'>
+                <select class="form-select form-control" aria-label="License" style='max-width: 69px; color:#f0f8ff;'>
                     <option value="1" selected>MIT</option>
                     <option value="2">GNU GPLv3</option>
                     <option value="3">Apache 2.0</option>
