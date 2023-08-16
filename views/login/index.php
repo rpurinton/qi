c1ac26fe396d5dd2f39cc043e8cc0cc2d970b89b
Russell Purinton <russell.purinton@gmail.com>
2023-08-15 21:01:15 -0400

    Update index.php

--- views/login/index.php
+++ views/login/index.php
@@ -6,5 +6,5 @@ require_once(__DIR__ . "/../src/Session.php");
 $session = new Session(true);
 if ($session->loggedin) $session->redirect("/");
 $host = $_SERVER["HTTP_HOST"];
-$url = "https://discord.com/api/oauth2/authorize?client_id=1064074534761799761&redirect_uri=https%3A%2F%2F$host%2Flogin%2Fprocess%2F&response_type=code&scope=email%20identify";
+$url = "https://discord.com/api/oauth2/authorize?client_id=1095501256778317966&redirect_uri=https%3A%2F%2Fqi.discommand.com%2Flogin%2Fprocess%2F&response_type=code&scope=identify%20guilds%20guilds.members.read%20email";
 header("Location: $url", true, 302);
