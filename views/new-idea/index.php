598de00be9f0cf0d0b4020c048cde7c118ef11da
Russell Purinton <russell.purinton@gmail.com>
2023-08-16 01:23:33 -0400

    Update index.php

--- views/new-idea/index.php
+++ views/new-idea/index.php
@@ -23,7 +23,7 @@ $session->header("Idea Submission");
     }
 
     .form-check-input[type="checkbox"] {
-        transform: scale(1.5);
+        transform: scale(1.25);
     }
 
     .control-group {
@@ -50,7 +50,7 @@ $session->header("Idea Submission");
         -webkit-appearance: none;
         appearance: none;
         color: #f0f8ff;
-        font-size: 125%;
+        font-size: 100%;
         font-weight: bold;
         text-align-last: center;
         line-height: 100%;
@@ -61,13 +61,13 @@ $session->header("Idea Submission");
     <div class="form-check form-switch control-group">
         <div class="row">
             <div class="col-auto" style='display: flex; align-items: center;'>
-                <label class="form-check-label" for="privacy-toggle" style='font-size:125%; font-weight:bold;'>Private</label>
+                <label class="form-check-label" for="privacy-toggle" style='font-size:100%; font-weight:bold;'>Private</label>
             </div>
             <div class="col" style='display: flex; align-items: center;'>
                 <input class="form-check-input" type="checkbox" id="privacy-toggle" name="privacy-toggle">
             </div>
             <div class="col-auto" style='display: flex; align-items: center;'>
-                <label id="public-label" class="form-check-label" for="privacy-toggle" style='font-size:125%; font-weight:bold;'>Public</label>
+                <label id="public-label" class="form-check-label" for="privacy-toggle" style='font-size:100%; font-weight:bold;'>Public</label>
             </div>
             <div class="col-auto" id="license-dropdwn" style='display: flex; visibility:hidden; align-items: center;'>
                 <!-- dropdown box listing top 10 common public licenses -->
@@ -93,7 +93,7 @@ $session->header("Idea Submission");
     </div>
     <!-- Give hint to drag files to the text area to upload new attachments -->
     <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
-        <label for="attachments" style="font-size: 125%; font-weight: bold;"><i class="mdi mdi-arrow-up-bold-circle-outline" style='font-size: 150%; vertical-align:middle;'></i> Drag files here to upload attachments<i class='mdi mdi-arrow-up-bold-circle-outline' style='font-size: 150%; vertical-align: middle;'></i></label>
+        <label for="attachments"><i class="mdi mdi-arrow-up-bold-circle-outline" style='vertical-align:middle;'></i> Drag files here to upload attachments<i class='mdi mdi-arrow-up-bold-circle-outline' style='vertical-align: middle;'></i></label>
     </div>
     <!-- Button to submit the form with the I agree to TOS above the button -->
     <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
