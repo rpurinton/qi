24bd9895e6b54541d9db7a046e8893dcdb039c03
Russell Purinton <russell.purinton@gmail.com>
2023-08-16 05:06:07 -0400

    Update index.php

--- views/idea/index.php
+++ views/idea/index.php
@@ -4,13 +4,18 @@ namespace Rpurinton\qi;
 
 require_once(__DIR__ . '/../src/Session.php');
 $session = new Session(false);
-$session->header("Idea ###");
+$idea_id = explode('/', $_SERVER['REQUEST_URI'])[2];
+extract($session->sql->single("SELECT min(`idea_id`) as `min_id`, max(`idea_id`) as `max_id` FROM `ideas`"));
+if (!is_numeric($idea_id) || $idea_id < $min_id || $idea_id > $max_id) {
+    $session->redirect('/search');
+}
+$session->header("Idea $idea_id");
 ?>
 <style>
 
 </style>
 <div class="discroller">
-    Individual Idea Page
+    <h1>Idea #<?= $idea_id ?></h1>
 </div>
 <script>
 
